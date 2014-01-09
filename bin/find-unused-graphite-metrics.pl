b6ff02b1477c547e9ea5cda4896c4099053acc7d
Rafael Garcia-Suarez <rgs@consttype.org>
2014-01-09 14:38:05 +0100

    First draft of a graphite enumerator module
    Includes an example perl script that lists all metrics under
    a specified namespace that have not been used to in the last day.

---
+++ bin/find-unused-graphite-metrics.pl
@@ -0,0 +1,40 @@
+#!perl
+
+# Finds all metrics in the general.metrics.* namespace that have
+# not been written to in the last day.
+
+use 5.14.1;
+use Graphite::Enumerator;
+use JSON;
+
+my $interval = '1day';
+my $verbose = 0;
+
+my $gren = Graphite::Enumerator->new(
+    host => 'https://graphite.example.com',
+    basepath => 'general.metrics',
+    lwp_options => {
+        env_proxy => 0,
+        keep_alive => 1,
+    },
+);
+
+$gren->enumerate( sub {
+    my ($path) = @_;
+    my $last_hour_data_url = $gren->host . "render/?format=json&from=-$interval&target=summarize($path,%22$interval%22,%22max%22,true)";
+    my $res = $gren->ua->get($last_hour_data_url);
+    if ($res->is_success) {
+        my $last_hour_data = decode_json($res->decoded_content);
+        return if !$last_hour_data || !@$last_hour_data;
+        my $datapoint = $last_hour_data->[0]{datapoints}[0][0];
+        if (defined $datapoint) {
+            say "> $path is still used" if $verbose;
+        }
+        else {
+            say "> $path is no longer used";
+        }
+    }
+    else {
+        $gren->log_warning("Can't get <$last_hour_data_url>: " . $res->status_line);
+    }
+} );
---
+++ lib/Graphite/Enumerator.pm
@@ -0,0 +1,73 @@
+package Graphite::Enumerator;
+
+use 5.14.1;
+use Carp qw/croak/;
+use LWP::UserAgent;
+use JSON;
+
+# Recognized constructor options:
+# - host (base URL)
+# - basepath (top-level metric to scan)
+# - lwp_options (hashref)
+
+sub new {
+    my ($class, %args) = @_;
+    $args{host} or croak "No host provided";
+    $args{host} =~ m{^https?://} or $args{host} = "http://".$args{host};
+    $args{host} =~ m{/$} or $args{host} .= '/';
+    if (defined $args{basepath}) {
+        $args{basepath} =~ /\.$/ or $args{basepath} .= '.';
+    }
+    else {
+        $args{basepath} = '';
+    }
+    $args{_finder} = $args{host} . 'metrics/find?format=completer&query=';
+    $args{_ua} = LWP::UserAgent->new( %{ $args{lwp_options} || {} } );
+    bless \%args, $class;
+}
+
+sub enumerate {
+    my ($self, $callback, $path) = @_;
+    $path //= $self->{basepath};
+    my $url = $self->{_finder} . $path;
+    my $res = $self->{_ua}->get($url);
+    if ($res->is_success) {
+        my $completer_answer = decode_json($res->decoded_content);
+        return 0 if !$completer_answer || !$completer_answer->{metrics};
+        for my $metric (@{ $completer_answer->{metrics} }) {
+            if ($metric->{is_leaf}) {
+                $callback->($metric->{path});
+            }
+            else {
+                $self->enumerate($callback, $metric->{path});
+            }
+        }
+        return 1;
+    }
+    else {
+        $self->log_warning("Can't get <$url>: " . $res->status_line);
+        return 0;
+    }
+}
+
+sub host {
+    my ($self) = @_;
+    return $self->{host};
+}
+
+sub ua {
+    my ($self) = @_;
+    return $self->{_ua};
+}
+
+sub log_message {
+    my ($self, $message) = @_;
+    print $message, "\n";
+}
+
+sub log_warning {
+    my ($self, $message) = @_;
+    warn $message, "\n";
+}
+
+1;
